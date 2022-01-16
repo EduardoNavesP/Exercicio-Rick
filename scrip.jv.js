4a9182135736915ade64dbdeed9c3516e85cc350
Eduardo Naves <eduardonavespetraglia@gmail.com>
2022-01-15 21:11:40 -0300

    Implementando linha de aleatorio

--- scrip.jv.js
+++ scrip.jv.js
@@ -1,25 +1,28 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const genetica = document.querySelector ('#genetica');
+const species = document.querySelector ('#species');
 const condicao = document.querySelector('#status');
 
 gerarValorAletorio = () => {
-    return Math.floor(Math.random() *5);
+    return Math.floor(Math.random() *10);
 }
+3
+
 pegarPersonagem = () => {
-    
-    return fetch('https://rickandmortyapi.com/api/character/${numeroAletorio}', {
-        method: 'GET',
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
+
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
+        species.innerHTML = data.species;
         condicao.innerHTML = data.status
     });
 }
